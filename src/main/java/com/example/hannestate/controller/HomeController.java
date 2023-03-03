65e473687062dd5764f385c28d7951ad16ffc0e2
Bishnu Dahal <bishnudahal@Bishnus-MacBook-Pro.local>
2023-03-03 18:17:08 -0500

    develop test

--- src/main/java/com/example/hannestate/controller/HomeController.java
+++ src/main/java/com/example/hannestate/controller/HomeController.java
@@ -9,6 +9,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @GetMapping("/")
     public String getHome() {
-        return "Success";
+        return "Success test";
     }
 }
