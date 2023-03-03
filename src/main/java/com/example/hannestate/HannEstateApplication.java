a7f8f9598ba33f572220495fea5f4c11c5940007
Bishnu Dahal <bishnudahal@Bishnus-MacBook-Pro.local>
2023-03-03 13:38:11 -0500

    initial

---
+++ src/main/java/com/example/hannestate/HannEstateApplication.java
@@ -0,0 +1,13 @@
+package com.example.hannestate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HannEstateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HannEstateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/hannestate/HannEstateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hannestate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HannEstateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
